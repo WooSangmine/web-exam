7e5fb70e74b730dc8d9959f4ad0f5903b035a29e
WooSangmine <110000782+WooSangmine@users.noreply.github.com>
2023-05-18 10:23:03 +0900

    a
    203

---
+++ js/script2.js
@@ -0,0 +1,17 @@
+jQuery(document).ready(function() {
+    $('.navi > li').mouseover(function() {
+        $('.submenu').stop().slideDown(500);
+    }).mouseout (function() {
+        $('.submenu').stop().slideUp(500);
+    });
+
+    setInterval(function(){
+        $('.slidelist').delay(2000);
+        $('.slidelist').animate({ marginLeft:-1200});
+        $('.slidelist').delay(2000);
+        $('.slidelist').animate({ marginLeft:-2400});
+        $('.slidelist').delay(2000);
+        $('.slidelist').animate({ marginLeft:0});
+        $('.slidelist').delay(2000);
+    });
+});
